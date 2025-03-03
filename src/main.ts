59fcd682a26a767e95d036034c2fee4c70439084
Mathis BOULARD <mathis.boulard@intra.gobelins.fr>
2025-03-03 16:25:46 +0100

    o "# test1" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Methesss/test1.git git push -u origin main# Please enter the commit message for your changes. Lines starting

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import{io} from 'socket.io-client';
+
+const socket = io("http://localhost:3000/");
+var pseudo: string | any[]
+
+const pseudoInput = document.getElementById('pseudoInput')!
+pseudoInput.addEventListener('input', changePseudoValue)
+
+function changePseudoValue(event){
+ pseudo = event.target.value;
+ console.log(event)
+}
+
+
+const joinButton = document.getElementById('joinButton')!
+joinButton.addEventListener("click", joinGame)
+
+function joinGame(){
+  if (pseudo.length < 1){
+    alert("Veuillez renseigner votre pseudo");
+  }
+  else{
+    socket.emit('joinGame', pseudo)
+
+  }
+}
+
+socket.on('joinGameStatus', displayStatus)
+function displayStatus(message: any){
+console.log(message)
+}
+
+
+
